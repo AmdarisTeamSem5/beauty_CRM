5cf0ee332d30be5f77ba4d5a04a2b767782b902f
Gheorghe2973 <gelu8793@gmail.com>
2025-09-10 20:19:50 +0300

    Created discovery page

---
+++ app/src/app/discovery/page.tsx
@@ -0,0 +1,9 @@
+export default function DiscoveryPage() {
+  return (
+    <div className="min-h-screen w-full bg-white">
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-gray-900 text-2xl font-semibold">Discovery Page</h1>
+      </div>
+    </div>
+  );
+}
