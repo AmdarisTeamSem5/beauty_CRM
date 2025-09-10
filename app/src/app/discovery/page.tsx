13a9ef87c8602e472255acc67a3aae9754ef8064
Gheorghe2973 <gelu8793@gmail.com>
2025-09-10 20:57:31 +0300

    Addded the filters component

--- app/src/app/discovery/page.tsx
+++ app/src/app/discovery/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+
+import SalonsFilters from '../../components/ui/filter';
+
 export default function DiscoveryPage() {
   return (
-    <div className="min-h-screen w-full bg-white">
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-gray-900 text-2xl font-semibold">Discovery Page</h1>
+    <div className="min-h-screen w-full bg-white flex">
+      <div className="p-6">
+        <SalonsFilters />
       </div>
     </div>
   );
