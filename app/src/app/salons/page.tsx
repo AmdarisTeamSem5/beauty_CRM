03185be2efdaa26cffef1555d1149340b98e502f
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-09-11 16:53:27 +0300

    removed unused dummy component

--- app/src/app/salons/page.tsx
+++ app/src/app/salons/page.tsx
@@ -1,18 +1,13 @@
 "use server";
 
 import SalonsFilters from "../../components/layout/salons/salonsFilter";
-import SalonCard from "@/components/layout/salons/salonCard";
 
 export default async function SalonsPage() {
   return (
     <div className="flex gap-x-10 my-12">
       <SalonsFilters />
       <div className="grid grid-cols-3 gap-8 w-full">
-        <SalonCard />
-        <SalonCard />
-        <SalonCard />
-        <SalonCard />
-        <SalonCard />
+        {/* salon cards */}
       </div>
     </div>
   );
