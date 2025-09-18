2c3f03f2aa607f38fb753e74a92f68221e2310da
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-09-18 16:05:35 +0300

    change context factory to allow fallback to appsettings when errors occur

--- backend/GNT/Shared/GNT.Infrastructure/Context/AppDbContextFactory.cs
+++ backend/GNT/Shared/GNT.Infrastructure/Context/AppDbContextFactory.cs
@@ -8,9 +8,23 @@ namespace GNT.Infrastructure.Context
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer(
-    "Data Source=.\\sqlexpress;Initial Catalog=CRM_Salons_DB;Integrated Security=SSPI;Persist Security Info=True;TrustServerCertificate=True;"
-            );
+            try
+            {
+                optionsBuilder.UseSqlServer(
+                "Data Source=.\\sqlexpress;Initial Catalog=CRM_Salons_DB;Integrated Security=SSPI;Persist Security Info=True;TrustServerCertificate=True;"
+                );
+            }
+            catch
+            {   // try another string
+                try
+                {
+                    optionsBuilder.UseSqlServer(
+                    "Server=localhost;Database=CRM_Salons_DB;Trusted_Connection=True;TrustServerCertificate=True;");
+                }
+                catch
+                {
+                }
+            }
 
             return new AppDbContext(optionsBuilder.Options);
         }
