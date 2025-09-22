1d4ab57b73e43f2a2210f14f1f26f1a04d070968
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-09-22 13:54:46 +0300

    fixed global margins for mobile

--- app/src/app/salons/page.tsx
+++ app/src/app/salons/page.tsx
@@ -6,7 +6,7 @@ import SalonSearch from "@/components/layout/salons/salonSearch";
 
 export default async function SalonsPage() {
   return (
-    <div className="w-full max-w-7xl mx-auto">
+    <div className="w-full mx-auto">
       <SalonSearch />
       <div className="w-full flex flex-col lg:flex-row gap-6 lg:gap-x-10 my-12">
         <SalonsFilters />
