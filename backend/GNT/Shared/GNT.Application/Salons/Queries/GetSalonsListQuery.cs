d3b9b99adc385c1d3da69852405e21cdf4cc5343
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-09-22 14:44:55 +0300

    added order by clause before .Distinct,removed unnecesary <?>

--- backend/GNT/Shared/GNT.Application/Salons/Queries/GetSalonsListQuery.cs
+++ backend/GNT/Shared/GNT.Application/Salons/Queries/GetSalonsListQuery.cs
@@ -2,7 +2,6 @@
 using GNT.Shared.Dtos.Salons;
 using GNT.Shared.Dtos.Pagination;
 using GNT.Shared.Enums;
-using System.ComponentModel.DataAnnotations;
 
 namespace GNT.Application.Salons.Queries;
 public class SalonListQuery : IRequest<PaginatedList<SalonDto>>
@@ -10,13 +9,13 @@ public class SalonListQuery : IRequest<PaginatedList<SalonDto>>
     public SalonListQuery(PageQuery pageQuery)
     {
         PageQuery = pageQuery;
-        SalonServiceTypes = pageQuery.Filters.FirstOrDefault(f => f.PropertyName == "ServiceTypes")?.Value; 
-        SalonSearch = pageQuery.Filters.FirstOrDefault(f => f.PropertyName == "Search")?.Value;   
+        SalonServiceTypes = pageQuery.Filters.FirstOrDefault(f => f.PropertyName == "ServiceTypes")?.Value;
+        SalonSearch = pageQuery.Filters.FirstOrDefault(f => f.PropertyName == "Search")?.Value;
     }
 
     internal PageQuery PageQuery { get; set; }
-    public string? SalonServiceTypes { get; set; }
-    public string? SalonSearch { get; set; }
+    public string SalonServiceTypes { get; set; }
+    public string SalonSearch { get; set; }
 }
 
 internal class SalonListQueryHandler : RequestHandler<SalonListQuery, PaginatedList<SalonDto>>
@@ -46,13 +45,14 @@ internal class SalonListQueryHandler : RequestHandler<SalonListQuery, PaginatedL
                     (s, ss) => new { Salon = s, SalonService = ss })
                 .Where(joined => serviceTypeIds.Contains(joined.SalonService.Type))
                 .Select(joined => joined.Salon)
+                .OrderBy(joined => joined.Id)
                 .Distinct();
         }
 
         if (!string.IsNullOrEmpty(request.SalonServiceTypes))
         {
             var valid_tokens = request.SalonSearch?.Split(" ").Where(invalid_token => invalid_token != " ");
-            query = query.Where(Salons => valid_tokens.All(token => Salons.Name.Contains(token)));
+            query = query.Where(Salons => valid_tokens.All(token => Salons.Name.Contains(token))).OrderBy(salon => salon.Id).Distinct();
         }
 
         var page = await paginationService.PaginatedResults(query, request.PageQuery, SalonMapping.DtoProjection);
