d326cdf049fab78cd45f3f6e0747c1232607ae80
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-09-24 17:44:47 +0300

    Added simple GET endpoints for Appointment, updated Dto to have

--- backend/GNT/Shared/GNT.Dtos/Dtos/Appointment/AppointmentDto.cs
+++ backend/GNT/Shared/GNT.Dtos/Dtos/Appointment/AppointmentDto.cs
@@ -3,13 +3,13 @@ namespace GNT.Shared.Dtos.Appointments;
 public class AppointmentDto
 {
     public Guid Id { get; set; }
-    // public Guid ClientId { get; set; } // the user who made the appointment
+    public Guid ClientId { get; set; } // the user who made the appointment
     // public User Client { get; set; }
     // public Guid SalonServiceId { get; set; } // the service for the appointment 
-    // public Guid SpecialistId { get; set; }
-    // public Guid SalonId { get; set; } // SalonService also contains the information about the salon so it doesn't need to be stored
+    public Guid SpecialistId { get; set; }
+    public Guid SalonId { get; set; } // SalonService also contains the information about the salon so it doesn't need to be stored
     // public DateTime AppointmentDate { get; set; }
-    // public bool Comfirmed { get; set; }
+    public bool Confirmed { get; set; }
     // public DateTime CreatedAt { get; set; }
     // public DateTime UpdatedAt { get; set; }
 
