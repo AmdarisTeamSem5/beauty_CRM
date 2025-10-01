1c534058463cb04807b0ceca1f5198610b4e2342
sabinapopescu <sabina.popescu@isa.utm.md>
2025-10-01 17:00:06 +0300

    cleaned files

--- backend/GNT/Shared/GNT.Infrastructure/Context/AppDbContextFactory.cs
+++ backend/GNT/Shared/GNT.Infrastructure/Context/AppDbContextFactory.cs
@@ -11,7 +11,8 @@ namespace GNT.Infrastructure.Context
             try
             {
                 optionsBuilder.UseSqlServer(
-"Server=127.0.0.1,1433;Database=CRM_Salons_DB;User Id=sa;Password=Str0ng!Pass;Encrypt=False;TrustServerCertificate=True;"                );
+                "Data Source=.\\sqlexpress;Initial Catalog=CRM_Salons_DB;Integrated Security=SSPI;Persist Security Info=True;TrustServerCertificate=True;"
+                );
             }
             catch
             {   // try another string
